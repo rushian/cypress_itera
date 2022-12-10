46cdb161b3edffb43cd29faad749ff383c761a69
Luciano <luciano.ivec@gmail.com>
2022-12-10 12:49:35 -0300

    aula finalizada

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     e2e: {
-        viewportHeight: 1080, // resolução de tela
-        viewportWidth: 1920,
+        viewportHeight: 660, // resolução de tela
+        viewportWidth: 1000,
         baseUrl: 'https://www.blazedemo.com', // endereço do software alvo
         // eslint-disable-next-line no-unused-vars
         setupNodeEvents(on, config) {
--- cypress/e2e/buscarVoos.cy.js
+++ cypress/e2e/buscarVoos.cy.js
@@ -1,26 +1,44 @@
-
 describe('Busca por voos', () => {
     context('nao logado', () => {
         const massaVoos = require('../fixtures/massaVoos')
 
         beforeEach(() => {
-            cy.visit('/')// acessa  base url
+            cy.visit('/') // acessa  base url
         })
         it('buscar voos entre san paolo e cairo - simples', () => {
             // verificar se estamos na pagina correta
-            cy.title().should('eq','BlazeDemo')
+            cy.title().should('eq', 'BlazeDemo')
             cy.get('select.form-inline')
                 .eq(0) // primeiro elemento com a class form-inline
                 .select('São Paolo')
             cy.get('select.form-inline')
                 .eq(1) // segundo elemento com a class form-inline
                 .select('Cairo')
-            cy.get('input.btn.btn-primary')
-                .click() // clica no botao selecionar voo
+            cy.get('input.btn.btn-primary').click() // clica no botao selecionar voo
             // validar titulo da guia
+            cy.title().should('eq', 'BlazeDemo - reserve')
 
             // validar frase de cabecalho
-            
+            cy.get('.container h3').should('have.text', 'Flights from São Paolo to Cairo: ')
         }) // fechar it
+
+        massaVoos.array.forEach(({ origem, destino }) => {
+            it(`buscar voos entre ${origem} e ${destino} a partir da massa`, () => {
+                // verificar se estamos na pagina correta
+                cy.title().should('eq', 'BlazeDemo')
+                cy.get('select.form-inline')
+                    .eq(0) // primeiro elemento com a class form-inline
+                    .select(origem)
+                cy.get('select.form-inline')
+                    .eq(1) // segundo elemento com a class form-inline
+                    .select(destino)
+                cy.get('input.btn.btn-primary').click() // clica no botao selecionar voo
+                // validar titulo da guia
+                cy.title().should('eq', 'BlazeDemo - reserve')
+
+                // validar frase de cabecalho
+                cy.get('.container h3').should('have.text', `Flights from ${origem} to ${destino}: `)
+            }) // fechar it
+        })
     }) // fechar context
 }) // fechar describe
